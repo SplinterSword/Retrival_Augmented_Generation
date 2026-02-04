b40bb659424d7dbd5a8f562ae0bb8eb710cfffb0
SplinterSword <sparshj2003@gmail.com>
2026-02-04 07:02:39 +0530

    Added dot product to vector_operations

--- utils/semantic_search_utils/vector_operations.py
+++ utils/semantic_search_utils/vector_operations.py
@@ -10,9 +10,16 @@ def subtract_vectors(v1, v2):
     
     return [a - b for a, b in zip(v1, v2)]
 
+def dot_product(v1, v2):
+    if len(v1) != len(v2):
+        raise ValueError("Vectors must have the same length")
+    
+    return sum(a * b for a, b in zip(v1, v2))
+
 
 if __name__ == "__main__":
     v1 = [1, 2, 3]
     v2 = [4, 5, 6]
     print(add_vectors(v1, v2))
     print(subtract_vectors(v1, v2))
+    print(dot_product(v1, v2))
